ea2202b7370f79253392a4db37fdeb8015cdf4ef
qgmartinez <75802342+qgmartinez@users.noreply.github.com>
2022-02-23 20:28:09 +0100

    Initial commit

---
+++ src/main/java/com/di/practicaConcesionario/PracticaConcesionarioApplication.java
@@ -0,0 +1,13 @@
+package com.di.practicaConcesionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaConcesionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaConcesionarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/di/practicaConcesionario/PracticaConcesionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.di.practicaConcesionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaConcesionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
